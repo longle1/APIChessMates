48ee95d40676e796c5e7997f831c326cc8ce1bde
longle1 <ltphilong2001@gmail.com>
2023-05-18 14:25:00 +0700

    update method post User lan 5

--- app/models/users.js
+++ app/models/users.js
@@ -20,7 +20,7 @@ module.exports = {
     },
 
     updateUser: async (params, res) => {
-        const user = await usersModel.findOne({userName: params.body.userName, _id: { $ne: ObjectId(params.id) }});
+        const user = await usersModel.findOne({userName: params.body.userName, _id: { $ne: params.id }});
         if(user) {
             res.status(401).json({
                 success: true,
